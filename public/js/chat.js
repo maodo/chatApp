e63acaf81eb25f2ebaa43c90e30b7df753ea4436
Maodo Sow <maodosw@gmail.com>
2020-02-23 15:59:06 +0000

    Chat deployment

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,27 +10,65 @@ const $messages = document.querySelector('#messages')
 // Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationTemplate = document.querySelector('#location-template').innerHTML
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 
 //Options
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true})
 
+const autoscroll = () => {
+    // New message element
+    const $newMessage = $messages.lastElementChild
+
+    // Height of the new message
+    const newMessageStyle = getComputedStyle($newMessage)
+    const newMessageMargin = parseInt(newMessageStyle.marginBottom)
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
+
+    // Visible height
+    const visibleHeight = $messages.offsetHeight
+
+    // Height of messages container
+    const containerHeight = $messages.scrollHeight
+
+    // How far have I scrolled ?
+    const scrollOffset = $messages.scrollTop + visibleHeight
+
+    if (containerHeight - newMessageHeight <= scrollOffset) {
+        $messages.scrollTop = $messages.scrollHeight
+    }
+}
+
+
 socket.on('message', (message) => {
     console.log(message)
     const html = Mustache.render(messageTemplate,{
+        username: message.username,
         message: message.text,
-        createdAt: moment(message.createdAt).format('H:mm A')
+        createdAt: moment(message.createdAt).format('H:mm A'),
+        me: 'me'
     })
     $messages.insertAdjacentHTML('beforeend',html)
+    autoscroll()
 
 })
 
 socket.on('location', (message) => {
     console.log(message)
     const html = Mustache.render(locationTemplate,{
+        username: message.username,
         location: message.location,
         createdAt: moment(message.createdAt).format('H:mm A')
     })
     $messages.insertAdjacentHTML('beforeend',html)
+    autoscroll()
+})
+
+socket.on('roomData', ({ room, users}) => {
+    const html = Mustache.render(sidebarTemplate,{
+        room,
+        users
+    })
+    document.querySelector('#sidebar').innerHTML = html
 })
 
 $messageForm.addEventListener('submit',(e) => {
@@ -67,5 +105,8 @@ $locationButton.addEventListener('click', () => {
 })
 
 socket.emit('join', { username, room}, (error) => {
-
+    if (error) {
+        alert(error)
+        location.href = '/'
+    }
 })
--- src/index.js
+++ src/index.js
@@ -29,26 +29,38 @@ io.on('connection', (socket) => {
 
         socket.join(user.room)
         
-        socket.emit('message',generateMessage('Welcome!'))
+        socket.emit('message',generateMessage('Admin','Welcome!'))
         socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`))
+        io.to(user.room).emit('roomData',{
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        })
 
         callback()
     })
 
     socket.on('sendMessage', (value, callback) => {
-        io.emit('message', generateMessage(value))
+        const user = getUser(socket.id)
+        socket.broadcast.to(user.room).emit('message', generateMessage(user.username,value))
+        // This allows to display 'me' instead of my username when i send a message
+        io.to(socket.id).emit('message',generateMessage('me',value))
         callback()
     })
 
     socket.on('sendLocation', (values, callback) => {
-        io.emit('location', generateLocationMessage(`https://www.google.com/maps?q=${values.lat},${values.lon}`))
+        const user = getUser(socket.id)
+        io.to(user.room).emit('location', generateLocationMessage(user.username,`https://www.google.com/maps?q=${values.lat},${values.lon}`))
         callback()
     })
 
     socket.on('disconnect', () => {
         const user = removeUser(socket.id)
         if (user) {
-            io.to(user.room).emit('message', generateMessage(`${user.username} has left !`))    
+            io.to(user.room).emit('message', generateMessage('Admin',`${user.username} has left !`))
+            io.to(user.room).emit('roomData',{
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            }) 
         }  
     })
 
